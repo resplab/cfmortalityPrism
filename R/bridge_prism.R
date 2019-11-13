8b1653f9893234e2088e7d1b4302a619479d2f1a
Adibi <maadibi@ead.ubc.ca>
2019-11-13 14:57:44 -0800

    bridge now functional

--- R/bridge_prism.R
+++ R/bridge_prism.R
@@ -7,21 +7,40 @@
 #' @param model_input A list/json object with "patient_data", "random_sampling_N", "random_distribution_iteration",
 #' and "calculate_CIs" as columns
 #' @return Returns a list of results
-model_run<-function(...)
+model_run<-function(model_input = NULL)
 {
 
+    input<-unflatten_list(model_input)
   
-    results <- predictCFMortality(...)
+    results <- predictCFMortality(age                    =model_input$age,                  
+                                  male                   =model_input$male,                  
+                                  fvc                    =model_input$fvc,                  
+                                  fev1                   =model_input$fev1,                  
+                                  fev1LastYear           =model_input$fev1LastYear,          
+                                  bcepacia               =model_input$bcepacia,             
+                                  underweight            =model_input$underweight,          
+                                  nHosp                  =model_input$nHosp,                
+                                  pancreaticInsufficient =model_input$pancreaticInsufficient,
+                                  CFRelatedDiabetes      =model_input$CFRelatedDiabetes,     
+                                  ageAtDiagnosis         =model_input$ageAtDiagnosis        )
     
     return(as.list(results))
 }
 
-
 get_default_input <- function() {
-  model_input <- list(age = 16, male = 0, fvc = 66.7, fev1 = 47.4, fev1LastYear = 80.5, bcepacia = 0, underweight = 0, nHosp = 0, pancreaicInsufficient = 1, CFRelatedDiabetes = 0, ageAtDiagnosis = 0.9)
-  return(flatten_list(model_input))
+  model_input <- list(age                    = 16, 
+                      male                   = 0, 
+                      fvc                    = 66.7, 
+                      fev1                   = 47.4, 
+                      fev1LastYear           = 80.5, 
+                      bcepacia               = 0, 
+                      underweight            = 0, 
+                      nHosp                  = 0, 
+                      pancreaticInsufficient = 1, 
+                      CFRelatedDiabetes      = 0, 
+                      ageAtDiagnosis         = 0.9)
+  return((flatten_list(model_input)))
 }
-
 
 
 #Gets a hierarchical named list and flattens it; updating names accordingly
@@ -50,5 +69,24 @@ flatten_list<-function(lst,prefix="")
       out[nm]<-lst[i]
     }
   }
+  return(out)
+}
+
+
+
+#Gets a hierarchical named list and flattens it; updating names accordingly
+unflatten_list<-function(lst)
+{
+  if(is.null(lst)) return(lst)
+  out<-list()
+  
+  nms<-names(lst)
+  
+  for(nm in nms)
+  {
+    path<-paste(strsplit(nm,'.',fixed=T)[[1]],sep="$")
+    eval(parse(text=paste("out$",paste(path,collapse="$"),"<-lst[[nm]]",sep="")))
+  }
+  
   return(out)
 }
